00065b3364615b6c06e18d3526e2c4d7afb21e9f
= <=>
2024-04-23 07:37:23 -0300

    add js da Sprint IV

---
+++ assets/js/script_noticia.js
@@ -0,0 +1,28 @@
+function validaNome(nome) {
+    const re = /^[a-zA-Z\s]*$/;
+    return re.test(nome);
+}
+
+
+form = document.getElementById('newslatter');
+nomeInput = document.getElementById('nome');
+
+
+form.addEventListener('submit', (event) => {
+    let temErro = false;
+
+    if (nomeInput.value.trim() === '') {
+        document.getElementById('nomeError').textContent = 'Por favor, preencha seu nome.';
+        temErro = true;
+    } else if (!validaNome(nomeInput.value.trim())) {
+        document.getElementById('nomeError').textContent = 'Por favor, insira um nome válido.';
+        temErro = true;
+    } else {
+        document.getElementById('nomeError').textContent = '';
+    }
+
+
+    if (temErro) {
+        event.preventDefault();
+    }
+})
