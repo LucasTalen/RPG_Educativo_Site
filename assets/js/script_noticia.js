8d4e43983a2e4c527cd5cb3b18aa69d782a981f3
= <=>
2024-05-07 06:38:12 -0300

    alter text

--- assets/js/script_noticia.js
+++ assets/js/script_noticia.js
@@ -15,7 +15,7 @@ form.addEventListener('submit', (event) => {
         document.getElementById('nomeError').textContent = 'Por favor, preencha seu nome.';
         temErro = true;
     } else if (!validaNome(nomeInput.value.trim())) {
-        document.getElementById('nomeError').textContent = ' o nome deve conter apenas letra.';
+        document.getElementById('nomeError').textContent = 'O nome deve conter apenas letra.';
         temErro = true;
     } else {
         document.getElementById('nomeError').textContent = '';
